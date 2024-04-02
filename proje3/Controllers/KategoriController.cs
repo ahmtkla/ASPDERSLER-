44a5ba61d0feea0bd8ba9f88394c354172c661f3
ahmtkla <esogu221120221012@gmail.com>
2024-04-03 00:05:41 +0300

    Proje dosyası ekle.

---
+++ proje3/Controllers/KategoriController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proje3.Models.Entity;
+
+namespace proje3.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }   
+    }
+}
---
+++ proje3/Controllers/MusteriController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proje3.Models.Entity;
+
+namespace proje3.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERİLER.ToList();
+            return View(degerler);
+        }   
+        [HttpGet]
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+    [HttpPost]
+    public ActionResult YeniMusteri(TBLMUSTERİLER p1)
+        {
+            db.TBLMUSTERİLER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
